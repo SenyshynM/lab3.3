b0648999776c384ae02ca872c7e133d79c7e555f
SenyshynM <73256299+SenyshynM@users.noreply.github.com>
2020-11-05 11:55:59 +0200

    1

---
+++ 3.3/3.3.cpp
@@ -0,0 +1,39 @@
+﻿// Lab_03_3.cpp
+// < Сенишин Михайло >
+// Лабораторна робота № 3.3
+// Розгалуження, задане графіком функції.
+// Варіант 10
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	double x, R, y; // вхідний аргумент, параметр, результат виразу
+	
+	cout << "R = "; cin >> R;
+	cout << "x = "; cin >> x;
+
+	// розгалуження в повній формі
+	if (x <= -8 - R)
+		y = R;
+	else
+		if (-8 - R < x && x < -8 + R)
+			y = sqrt(2 * pow(R, 2) - pow(x, 2) - 16 * x - 64);
+		else
+			if (-8 + R < x && x <= -4)
+				y = R;
+			else
+				if (-4 < x && x <= 2)
+					y = R - (1 + R) / 6 * (x + 4);
+				else
+					y = x-3;
+	
+	cout << endl;
+	cout << "y = " << y << endl;
+	
+	cin.get();
+	return 0;
+}
